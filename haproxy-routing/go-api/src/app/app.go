1d6758d305708ef4435e5fdaf8a3995101796cc3
egenerat <emile.generat@outlook.com>
2019-03-02 23:15:22 +0000

    haproxy routing

---
+++ haproxy-routing/go-api/src/app/app.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handlerApi(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"message": "Hello world"}`)
+}
+
+func main() {
+	http.HandleFunc("/api", handlerApi)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
